7b1961e89f90d38dce0c9bfa6c50d6de13b8e683
James Zhou <yunchengzhou@gmail.com>
2026-05-17 17:10:56 -0500

    117. Populating Next Right Pointers in Each Node II

---
+++ leetcode-med/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,31 @@
+#include "leet.h"
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        vector<Node*> lastNodes = {nullptr};
+        int depth = 0;
+        helper(root, lastNodes, depth);
+        return root;
+
+    }
+    void helper(Node* root, vector<Node*>& lastNodes, int depth) {
+        if (root == nullptr) {
+            return;
+        }
+        if (lastNodes.size()-1 < depth) {
+            lastNodes.push_back(root);
+        } else {
+            if (lastNodes[depth] != nullptr) {
+                lastNodes[depth]->next = root;
+            }
+            lastNodes[depth] = root;
+        }
+        helper(root->left, lastNodes, depth+1);
+        helper(root->right, lastNodes, depth+1);
+    }
+    
+};
